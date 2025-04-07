8dcf4279a88a8fc7a5314ac20522e852cf5ab68c
Himanshu02 <91370829+Himanshu0273@users.noreply.github.com>
2025-04-07 17:40:30 +0530

    UC1- First Name Valid

--- user_registration_system.py
+++ user_registration_system.py
@@ -1,7 +1,7 @@
 import re
 
 def valid_first_name(s):
-    if re.match("Cap([a-z] | [A-Z])", s):
+    if re.match(r"^[A-Z][a-zA-Z]{2,}$", s):
         return True     
     else:
         return False
@@ -10,4 +10,4 @@ first_name=input("Enter Valid First Name: ")
 if valid_first_name(first_name):
     print("Valid First Name, Continue ahead!!")
 else:
-    print("First name is invalid, does not start with 'Cap'")
+    print("First name is invalid.")
