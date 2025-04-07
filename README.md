cb2be408dd34c25c89d67bf7aad59ab64d0551ba
Himanshu02 <91370829+Himanshu0273@users.noreply.github.com>
2025-04-07 17:57:21 +0530

    UC2-Last Name

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# User-Registration-System" 
+Last Name Validation
+This project validates a user's last name based on specific rules.
+
+The last name must start with a capital letter and contain at least three alphabetic characters. It should not include any digits or special characters.
+
+The regular expression used for validation is: ^[A-Z][a-zA-Z]{2,}$.
+
+Some examples of valid last names are "Kumar" and "Sharma". Examples of invalid last names include "kumar" (starts with a lowercase letter), "Ku" (less than three characters), and "Kumar3" (contains a digit).
--- user_registration_system.py
+++ user_registration_system.py
@@ -1,13 +1,24 @@
 import re
 
-def valid_first_name(s):
+def valid_name(s):
     if re.match(r"^[A-Z][a-zA-Z]{2,}$", s):
         return True     
     else:
         return False
     
-first_name=input("Enter Valid First Name: ")
-if valid_first_name(first_name):
+first_name=input("Enter the First Name: ")
+#Check first name
+if valid_name(first_name):
     print("Valid First Name, Continue ahead!!")
+    
+    #Check Last Name
+    last_name = input("Enter the Last Name: ")
+    if valid_name(last_name):
+        print("Valid Last Name, Continue ahead!!")
+        
+    else:
+        print("Last name is invalid!")
+        
+        
 else:
-    print("First name is invalid.")
+    print("First name is invalid!")
