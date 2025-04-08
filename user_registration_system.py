784f7a14b2c05adc0aedbb6c07be47270387d542
Himanshu02 <91370829+Himanshu0273@users.noreply.github.com>
2025-04-08 14:11:18 +0530

    Checks for valid email

--- user_registration_system.py
+++ user_registration_system.py
@@ -1,11 +1,19 @@
 import re
 
+#Function for valid name:
 def valid_name(s):
     if re.match(r"^[A-Z][a-zA-Z]{2,}$", s):
         return True     
     else:
         return False
     
+#Function for valid email:
+def valid_email(s):
+    pattern = r"^[\w]+(\.[\w]+)?@[\w]+\.(com|co(\.in)?)$"
+    if re.match(pattern, s):
+        return True
+    return False
+
 first_name=input("Enter the First Name: ")
 #Check first name
 if valid_name(first_name):
@@ -15,6 +23,14 @@ if valid_name(first_name):
     last_name = input("Enter the Last Name: ")
     if valid_name(last_name):
         print("Valid Last Name, Continue ahead!!")
+        
+        #Check Valid Email
+        email = input("Enter your email address: ")
+        if valid_email(email):
+            print("Email Valid!!")
+            
+        else:
+            print("Invalid Email!")
         
     else:
         print("Last name is invalid!")
